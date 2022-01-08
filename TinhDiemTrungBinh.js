9d22859b3784697d390fe6d431601f9fd3932208
unknown <helloworld.vn2112@gmail.com>
2022-01-08 16:52:36 +0700

    update

--- TinhDiemTrungBinh.js
+++ TinhDiemTrungBinh.js
@@ -1,11 +1,10 @@
-var diemTrungBinh = (diemToan, diemLy ,diemHoa)=>{
-    var diemTrungBinh = 0;
+var diemTrungBinh = (diemToan, diemLy, diemHoa) => {
+  var diemTrungBinh = 0;
 
-    diemTrungBinh = (diemToan + diemLy + diemHoa) / 3;
+  diemTrungBinh = (diemToan + diemLy + diemHoa) / 3;
 
-    return diemTrungBinh;
-}
+  return diemTrungBinh;
+};
 
-
-var result = diemTrungBinh(9,9,9);
+var result = diemTrungBinh(9, 9, 9);
 console.log(result);
