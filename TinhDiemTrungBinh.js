24de4b9899d6ec8cae6e817d7e10d099c49b10da
unknown <helloworld.vn2112@gmail.com>
2022-01-08 15:48:28 +0700

    update

---
+++ TinhDiemTrungBinh.js
@@ -0,0 +1,11 @@
+var diemTrungBinh = (diemToan, diemLy ,diemHoa)=>{
+    var diemTrungBinh = 0;
+
+    diemTrungBinh = (diemToan + diemLy + diemHoa) / 3;
+
+    return diemTrungBinh;
+}
+
+
+var result = diemTrungBinh(9,9,9);
+console.log(result);
---
+++ if_else.js
@@ -0,0 +1,15 @@
+console.log(`Nhập số từ 1-5:`);
+let n = 1;
+if (n == 1) {
+  console.log(`Một`);
+} else if (n == 2) {
+  console.log(`Hai`);
+} else if (n == 3) {
+  console.log(`Ba`);
+} else if (n == 4) {
+  console.log(`Bốn`);
+} else if (n == 5) {
+  console.log(`Năm`);
+} else {
+  console.log(`Invalid. Try again.`);
+}
