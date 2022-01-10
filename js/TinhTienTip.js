9831d9ff61fd7d751010b41d1ab5de3bac5c6550
Khoa Nguyen <helloworld.vn2112@gmail.com>
2022-01-10 10:14:26 +0700

    updated script src

--- js/TinhTienTip.js
+++ js/TinhTienTip.js
@@ -1,13 +1,10 @@
 
-
-
-
 document.getElementById("btnTinhTien").onclick = () => {
     var tongTien = document.querySelector('#tongTien').value;
     var phanTramTip = document.querySelector("#phanTramTip").value; 
     var soNguoi = document.querySelector('#soNguoi').value;
     
-    var tienTipTrenNguoi = tongTien * phanTramTip / 100 / soNguoi;  
+    var tienTipTrenNguoi = (tongTien * phanTramTip / 100) / soNguoi;  
     
     document.querySelector("#tienTip").innerHTML = tienTipTrenNguoi.toFixed(2);
     
